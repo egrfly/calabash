f3559cd2b86c34c29368b074ca833ace1dfbf98c
egrfly <egrfly@outlook.com>
2025-03-09 22:55:40 +0000

    Separate out signal handler installation files

--- src/execute/signals/break_if_sigint_received.c
+++ src/execute/signals/break_if_sigint_received.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   install_signal_handlers.c                          :+:      :+:    :+:   */
+/*   break_if_sigint_received.c                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: emflynn <emflynn@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/06 13:41:31 by aistok            #+#    #+#             */
-/*   Updated: 2025/03/09 22:55:17 by emflynn          ###   ########.fr       */
+/*   Created: 2025/03/09 22:54:32 by emflynn           #+#    #+#             */
+/*   Updated: 2025/03/09 22:55:03 by emflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,9 @@
 #include <readline/readline.h>
 #include "./signals.h"
 
-void	install_signal_handlers(void)
+int	break_if_sigint_received(void)
 {
-	rl_event_hook = break_if_sigint_received;
-	install_signal_handler(SIGINT, sigint_handler);
-	install_signal_handler(SIGQUIT, sigquit_handler);
+	if (g_signal == SIGINT)
+		rl_done = true;
+	return (0);
 }
