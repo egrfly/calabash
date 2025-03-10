3031678449b6c5719f965da83f178cdf2a142fb8
egrfly <egrfly@outlook.com>
2025-03-10 09:44:55 +0000

    Add redirections for groups and subshells

--- src/execute/command_utils/destroy_exec_params.c
+++ src/execute/command_utils/destroy_exec_params.c
@@ -6,7 +6,7 @@
 /*   By: emflynn <emflynn@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/06 03:09:39 by emflynn           #+#    #+#             */
-/*   Updated: 2025/03/06 03:10:27 by emflynn          ###   ########.fr       */
+/*   Updated: 2025/03/10 09:39:57 by emflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@
 void	destroy_exec_params(
 			t_exec_params *exec_params)
 {
+	if (!exec_params)
+		return ;
 	free(exec_params->path);
 	free(exec_params->args);
 	free(exec_params->envp);
--- src/execute/execution_funcs/execute_group.c
+++ src/execute/execution_funcs/execute_group.c
@@ -6,20 +6,31 @@
 /*   By: emflynn <emflynn@student.42london.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 21:24:48 by emflynn           #+#    #+#             */
-/*   Updated: 2025/03/10 05:57:07 by emflynn          ###   ########.fr       */
+/*   Updated: 2025/03/10 09:44:50 by emflynn          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_binary_tree.h"
+#include "../../main.h"
 #include "../../interface/interface.h"
 #include "../execute.h"
+#include "../command_utils/command_utils.h"
+#include "../redirection_utils/redirection_utils.h"
 
-// TODO: these can have redirections
 int	execute_group(
 		t_binary_tree_node *node,
 		t_tokens_and_syntax_tree *tokens_and_syntax_tree,
 		t_program_vars *program_vars)
 {
-	return (execute_recursively(node->primary_child, tokens_and_syntax_tree,
-			program_vars));
+	t_syntax_tree_node_value	*node_value;
+	int							exit_status;
+
+	node_value = node->value;
+	if (!perform_redirections(node_value->redirections))
+		exit_due_to_redirection_failure(program_vars, NO_ARG,
+			tokens_and_syntax_tree);
+	exit_status = execute_recursively(node->primary_child,
+			tokens_and_syntax_tree, program_vars);
+	revert_redirections(node_value->redirections);
+	return (exit_status);
 }
