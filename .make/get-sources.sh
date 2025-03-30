d4ca3f4664b591d5967bd87b2e0918c4aa7ca162
Attila <attila.london@ymail.com>
2025-03-30 03:15:58 +0100

    updated get-sources.sh

--- .make/get-sources.sh
+++ .make/get-sources.sh
@@ -4,10 +4,8 @@ SRC_PATH=./src
 
 if [ $1 ] && [ -d $1 ]; then
 	SRC_PATH=$1;
-else
-	if [ $1 ]; then
-		echo "ERROR: $1 is not a directory (using $SRC_PATH instead)." >&2
-	fi
+elif [ $1 ]; then
+	echo "ERROR: $1 is not a directory (using $SRC_PATH instead)." >&2
 fi
 
 echo "SRC_DIRS := \\"
@@ -16,6 +14,6 @@ find $SRC_PATH -type d -exec echo -e "\t\t\t\t\t{} \\" \;
 echo
 
 echo "SRCS := \\"
-find $SRC_PATH/ -name "*.c" -exec echo -e "\t\t\t\t\t{} \\" \;
+find $SRC_PATH -type f -name "*.c" -exec echo -e "\t\t\t\t\t{} \\" \;
 
 echo
