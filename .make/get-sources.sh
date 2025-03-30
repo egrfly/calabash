cd0e808baa1a7266d7c1ad6849515dff4e520d76
Attila <attila.london@ymail.com>
2025-03-30 02:56:51 +0100

    added get-sources.sh

---
+++ .make/get-sources.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SRC_PATH=./src
+
+if [ $1 ] && [ -d $1 ]; then
+	SRC_PATH=$1;
+else
+	if [ $1 ]; then
+		echo "ERROR: $1 is not a directory (using $SRC_PATH instead)." >&2
+	fi
+fi
+
+echo "SRC_DIRS :=	"
+find $SRC_PATH -type d -exec echo "			{}" \;
+
+echo
+
+echo "SRCS :=		"
+find $SRC_PATH/ -name "*.c" -exec echo "			{}" \;
+
+echo
